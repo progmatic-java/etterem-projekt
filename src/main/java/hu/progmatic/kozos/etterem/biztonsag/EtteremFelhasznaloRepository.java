796eebe8651b323cd82386d2995c36e734041828
benibogyo <benjamin.katona@yahoo.com>
2022-02-09 13:29:32 +0100

    biztonság első lépések

--- src/main/java/hu/progmatic/kozos/etterem/biztonsag/EtteremFelhasznaloRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.progmatic.kozos.etterem.biztonsag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface EtteremFelhasznaloRepository extends JpaRepository<EtteremFelhasznalo, Long> {
-    Optional<EtteremFelhasznalo> findByNev(String nev);
-}
