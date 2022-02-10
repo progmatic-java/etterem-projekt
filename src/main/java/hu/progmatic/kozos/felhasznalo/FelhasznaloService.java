9f1ae1cb42a19fd59975b0f7623eef22ce42899d
benibogyo <benjamin.katona@yahoo.com>
2022-02-10 10:42:37 +0100

    felhasználók listában

--- src/main/java/hu/progmatic/kozos/felhasznalo/FelhasznaloService.java
+++ src/main/java/hu/progmatic/kozos/felhasznalo/FelhasznaloService.java
@@ -16,67 +16,71 @@ import java.util.Optional;
 @Transactional
 public class FelhasznaloService implements InitializingBean {
 
-  private final FelhasznaloRepository felhasznaloRepository;
-  private final PasswordEncoder encoder;
+    private final FelhasznaloRepository felhasznaloRepository;
+    private final PasswordEncoder encoder;
 
-  public FelhasznaloService(FelhasznaloRepository felhasznaloRepository, PasswordEncoder encoder) {
-    this.felhasznaloRepository = felhasznaloRepository;
-    this.encoder = encoder;
-  }
-
-  @RolesAllowed(UserType.Roles.USER_READ_ROLE)
-  public List<Felhasznalo> findAll() {
-    return felhasznaloRepository.findAll();
-  }
-
-  @RolesAllowed(UserType.Roles.USER_WRITE_ROLE)
-  public void add(UjFelhasznaloCommand command) {
-    if (felhasznaloRepository.findByNev(command.getNev()).isPresent()) {
-      throw new FelhasznaloLetrehozasException("Ilyen névvel már létezik felhasználó!");
+    public FelhasznaloService(FelhasznaloRepository felhasznaloRepository, PasswordEncoder encoder) {
+        this.felhasznaloRepository = felhasznaloRepository;
+        this.encoder = encoder;
     }
-    Felhasznalo felhasznalo = Felhasznalo.builder()
-        .nev(command.getNev())
-        .jelszo(encoder.encode(command.getJelszo()))
-        .role(command.getRole())
-        .build();
-    felhasznaloRepository.save(felhasznalo);
-  }
 
-  @RolesAllowed(UserType.Roles.USER_WRITE_ROLE)
-  public void delete(Long id) {
-    felhasznaloRepository.deleteById(id);
-  }
-
-  @RolesAllowed(UserType.Roles.USER_READ_ROLE)
-  public Optional<Felhasznalo> findByName(String nev) {
-    return felhasznaloRepository.findByNev(nev);
-  }
-
-  @Override
-  public void afterPropertiesSet() {
-    if (findAll().isEmpty()) {
-      add(new UjFelhasznaloCommand("admin", "adminpass", UserType.ADMIN));
-      add(new UjFelhasznaloCommand("bence", "bence", UserType.ADMIN));
-      add(new UjFelhasznaloCommand("benji", "benji", UserType.ADMIN));
-      add(new UjFelhasznaloCommand("attila", "attila", UserType.ADMIN));
-      add(new UjFelhasznaloCommand("olivér", "olivér", UserType.ADMIN));
-      add(new UjFelhasznaloCommand("dávid", "dávid", UserType.ADMIN));
-      add(new UjFelhasznaloCommand("miska", "miska", UserType.ADMIN));
-      add(new UjFelhasznaloCommand("felszolgáló", "felszolgáló", UserType.FELSZOLGALO));
+    @RolesAllowed(UserType.Roles.USER_READ_ROLE)
+    public List<Felhasznalo> findAll() {
+        return felhasznaloRepository.findAll();
     }
-  }
 
-  public boolean hasRole(String role) {
-    MyUserDetails userPrincipal = getMyUserDetails();
-    return userPrincipal.getRole().hasRole(role);
-  }
+    @RolesAllowed(UserType.Roles.USER_WRITE_ROLE)
+    public void add(UjFelhasznaloCommand command) {
+        if (felhasznaloRepository.findByNev(command.getNev()).isPresent()) {
+            throw new FelhasznaloLetrehozasException("Ilyen névvel már létezik felhasználó!");
+        }
+        Felhasznalo felhasznalo = Felhasznalo.builder()
+                .nev(command.getNev())
+                .jelszo(encoder.encode(command.getJelszo()))
+                .role(command.getRole())
+                .build();
+        felhasznaloRepository.save(felhasznalo);
+    }
 
-  public Long getFelhasznaloId() {
-    MyUserDetails userPrincipal = getMyUserDetails();
-    return userPrincipal.getFelhasznaloId();
-  }
+    @RolesAllowed(UserType.Roles.USER_WRITE_ROLE)
+    public void delete(Long id) {
+        felhasznaloRepository.deleteById(id);
+    }
 
-  private MyUserDetails getMyUserDetails() {
-    return (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-  }
+    @RolesAllowed(UserType.Roles.USER_READ_ROLE)
+    public Optional<Felhasznalo> findByName(String nev) {
+        return felhasznaloRepository.findByNev(nev);
+    }
+
+    private final List<UjFelhasznaloCommand> felhasznalok = List.of(
+            new UjFelhasznaloCommand("admin", "adminpass", UserType.ADMIN),
+            new UjFelhasznaloCommand("bence", "bence", UserType.ADMIN),
+            new UjFelhasznaloCommand("benji", "benji", UserType.ADMIN),
+            new UjFelhasznaloCommand("attila", "attila", UserType.ADMIN),
+            new UjFelhasznaloCommand("olivér", "olivér", UserType.ADMIN),
+            new UjFelhasznaloCommand("dávid", "dávid", UserType.ADMIN),
+            new UjFelhasznaloCommand("miska", "miska", UserType.ADMIN),
+            new UjFelhasznaloCommand("felszolgáló", "felszolgáló", UserType.FELSZOLGALO)
+    );
+
+    @Override
+    public void afterPropertiesSet() {
+        if (findAll().isEmpty()) {
+            felhasznalok.forEach(this::add);
+        }
+    }
+
+    public boolean hasRole(String role) {
+        MyUserDetails userPrincipal = getMyUserDetails();
+        return userPrincipal.getRole().hasRole(role);
+    }
+
+    public Long getFelhasznaloId() {
+        MyUserDetails userPrincipal = getMyUserDetails();
+        return userPrincipal.getFelhasznaloId();
+    }
+
+    private MyUserDetails getMyUserDetails() {
+        return (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
 }
