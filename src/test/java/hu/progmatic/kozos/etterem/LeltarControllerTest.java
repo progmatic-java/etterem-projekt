0b3ee557cabbce2c178f5f0acaaa52375ede5846
attilafarmasi <attila.farmasi@gmail.com>
2022-02-11 14:36:51 +0100

    teszt

--- src/test/java/hu/progmatic/kozos/etterem/LeltarControllerTest.java
+++ src/test/java/hu/progmatic/kozos/etterem/LeltarControllerTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,5 +31,14 @@ public class LeltarControllerTest {
                         MockMvcRequestBuilders.get("/etterem/leltar?")
                 ).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Leltár")));
+    }
+
+    @Test
+    @DisplayName("Termék szerkesztése")
+    void szerkesztes() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/etterem/leltar/10?")
+                ).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Paradicsom leves")));
     }
 }
