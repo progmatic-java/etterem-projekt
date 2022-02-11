77d90b1c8139cabded9d46ea0cc2ded98caf2590
attilafarmasi <attila.farmasi@gmail.com>
2022-02-11 13:49:48 +0100

    teszt

--- src/test/java/hu/progmatic/kozos/etterem/RendelesServiceTest.java
+++ src/test/java/hu/progmatic/kozos/etterem/RendelesServiceTest.java
@@ -24,94 +24,94 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class RendelesServiceTest {
-  @Autowired
-  RendelesService rendelesService;
-  @Autowired
-  AsztalService asztalService;
-  @Autowired
-  TermekService termekService;
+    @Autowired
+    RendelesService rendelesService;
+    @Autowired
+    AsztalService asztalService;
+    @Autowired
+    TermekService termekService;
 
-  private Asztal tesztAsztal;
-  private Termek tesztTermek;
-  private CreateRendelesCommand command;
+    private Asztal tesztAsztal;
+    private Termek tesztTermek;
+    private CreateRendelesCommand command;
 
-  @BeforeEach
-  void setUp() {
-    tesztAsztal = asztalService.getById(asztalService.getIdByNev("1. ASZTAL"));
-    tesztTermek = termekService.getByName("Paradicsom leves");
-    command = CreateRendelesCommand.builder()
-        .asztalId(tesztAsztal.getId())
-        .etteremTermekId(tesztTermek.getId())
-        .mennyiseg(1)
-        .build();
-  }
+    @BeforeEach
+    void setUp() {
+        tesztAsztal = asztalService.getById(asztalService.getIdByNev("1. ASZTAL"));
+        tesztTermek = termekService.getByName("Paradicsom leves");
+        command = CreateRendelesCommand.builder()
+                .asztalId(tesztAsztal.getId())
+                .etteremTermekId(tesztTermek.getId())
+                .mennyiseg(1)
+                .build();
+    }
 
-  @AfterEach
-  void tearDown() {
-    tesztAsztal.setRendelesek(new ArrayList<>());
-  }
+    @AfterEach
+    void tearDown() {
+        tesztAsztal.setRendelesek(new ArrayList<>());
+    }
 
-  @Test
-  @DisplayName("Rendelés hozzáadása asztalhoz")
-  void addOrderToAsztalTest() {
-    Rendeles tesztOrder = rendelesService.create(command);
-    TableViewDto dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
-    assertEquals("1. ASZTAL", dto.getNev());
-    assertEquals(tesztAsztal.getId(), dto.getId());
-    assertThat(dto.getRendelesDtoList()).hasSize(1);
-    TableViewDto.RendelesDto order = dto.getRendelesDtoList().get(0);
-    assertEquals("Paradicsom leves", order.getEtteremTermekNev());
-    assertEquals(1, order.getMennyiseg());
-    assertEquals(tesztOrder.getId(), order.getRendelesId());
-  }
+    @Test
+    @DisplayName("Rendelés hozzáadása asztalhoz")
+    void addOrderToAsztalTest() {
+        Rendeles tesztOrder = rendelesService.create(command);
+        TableViewDto dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
+        assertEquals("1. ASZTAL", dto.getNev());
+        assertEquals(tesztAsztal.getId(), dto.getId());
+        assertThat(dto.getRendelesDtoList()).hasSize(1);
+        TableViewDto.RendelesDto order = dto.getRendelesDtoList().get(0);
+        assertEquals("Paradicsom leves", order.getEtteremTermekNev());
+        assertEquals(1, order.getMennyiseg());
+        assertEquals(tesztOrder.getId(), order.getRendelesId());
+    }
 
-  @Test
-  @DisplayName("Mennyiség hozzáadása termék névvel")
-  void mennyisegHozzaadasaTermekNevvelTest() {
-    rendelesService.create(command);
-    TableViewDto dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
-    TableViewDto.RendelesDto rendelesDto = dto.getRendelesDtoList().stream()
-        .findFirst()
-        .orElseThrow();
-    assertEquals("Paradicsom leves", rendelesDto.getEtteremTermekNev());
-    assertEquals(1, rendelesDto.getMennyiseg());
-    rendelesService.mennyisegNovelese(tesztAsztal.getId(), tesztTermek.getNev());
-    dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
-    rendelesDto = dto.getRendelesDtoList().stream()
-        .findFirst()
-        .orElseThrow();
-    assertEquals("Paradicsom leves", rendelesDto.getEtteremTermekNev());
-    assertEquals(2, rendelesDto.getMennyiseg());
-  }
+    @Test
+    @DisplayName("Mennyiség hozzáadása termék névvel")
+    void mennyisegHozzaadasaTermekNevvelTest() {
+        rendelesService.create(command);
+        TableViewDto dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
+        TableViewDto.RendelesDto rendelesDto = dto.getRendelesDtoList().stream()
+                .findFirst()
+                .orElseThrow();
+        assertEquals("Paradicsom leves", rendelesDto.getEtteremTermekNev());
+        assertEquals(1, rendelesDto.getMennyiseg());
+        rendelesService.mennyisegNovelese(tesztAsztal.getId(), tesztTermek.getNev());
+        dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
+        rendelesDto = dto.getRendelesDtoList().stream()
+                .findFirst()
+                .orElseThrow();
+        assertEquals("Paradicsom leves", rendelesDto.getEtteremTermekNev());
+        assertEquals(2, rendelesDto.getMennyiseg());
+    }
 
-  @Test
-  @DisplayName("Mennyiség hozzáadása termék Id-val")
-  void mennyisegHozzaadasaTermekIdvalTest() {
-    rendelesService.create(command);
-    TableViewDto dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
-    TableViewDto.RendelesDto rendelesDto = dto.getRendelesDtoList().stream()
-        .findFirst()
-        .orElseThrow();
-    assertEquals("Paradicsom leves", rendelesDto.getEtteremTermekNev());
-    assertEquals(1, rendelesDto.getMennyiseg());
-    rendelesService.mennyisegNovelese(tesztAsztal.getId(), tesztTermek.getId());
-    dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
-    rendelesDto = dto.getRendelesDtoList().stream()
-        .findFirst()
-        .orElseThrow();
-    assertEquals("Paradicsom leves", rendelesDto.getEtteremTermekNev());
-    assertEquals(2, rendelesDto.getMennyiseg());
-  }
+    @Test
+    @DisplayName("Mennyiség hozzáadása termék Id-val")
+    void mennyisegHozzaadasaTermekIdvalTest() {
+        rendelesService.create(command);
+        TableViewDto dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
+        TableViewDto.RendelesDto rendelesDto = dto.getRendelesDtoList().stream()
+                .findFirst()
+                .orElseThrow();
+        assertEquals("Paradicsom leves", rendelesDto.getEtteremTermekNev());
+        assertEquals(1, rendelesDto.getMennyiseg());
+        rendelesService.mennyisegNovelese(tesztAsztal.getId(), tesztTermek.getId());
+        dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
+        rendelesDto = dto.getRendelesDtoList().stream()
+                .findFirst()
+                .orElseThrow();
+        assertEquals("Paradicsom leves", rendelesDto.getEtteremTermekNev());
+        assertEquals(2, rendelesDto.getMennyiseg());
+    }
 
     @Test
     @DisplayName("Mennyiség csökkentése termék névvel")
     void mennyisegcsokkenteseTermekNevvelTest() {
-      command.setMennyiseg(2);
+        command.setMennyiseg(2);
         rendelesService.create(command);
         TableViewDto dto = asztalService.getTableViewDto(tesztAsztal.getId(), Tipus.LEVES);
         TableViewDto.RendelesDto rendelesDto = dto.getRendelesDtoList().stream()
@@ -128,14 +128,22 @@ class RendelesServiceTest {
         assertEquals(1, rendelesDto.getMennyiseg());
     }
 
-  @Test
-  @DisplayName("Rendelés keresése asztal alapján")
-  void findAllByAsztalTest() {
-    rendelesService.create(command);
-    List<Rendeles> rendelesek = rendelesService.findAllByAsztal(tesztAsztal);
-    assertThat(rendelesek)
-        .extracting(Rendeles::getAsztal)
-        .extracting(Asztal::getNev)
-        .containsExactlyInAnyOrder("1. ASZTAL");
-  }
+    @Test
+    @DisplayName("Rendelés keresése asztal alapján")
+    void findAllByAsztalTest() {
+        rendelesService.create(command);
+        List<Rendeles> rendelesek = rendelesService.findAllByAsztal(tesztAsztal);
+        assertThat(rendelesek)
+                .extracting(Rendeles::getAsztal)
+                .extracting(Asztal::getNev)
+                .containsExactlyInAnyOrder("1. ASZTAL");
+    }
+
+    @Test
+    @DisplayName("Rendelés tartalmazza a terméket")
+    void rendelesTartalmazzaTest() {
+        assertFalse(rendelesService.rendelesTartalmazzaATermeket(command));
+        rendelesService.create(command);
+        assertTrue(rendelesService.rendelesTartalmazzaATermeket(command));
+    }
 }
