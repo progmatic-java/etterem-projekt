aa45820d766bac38269902fa5344a3ab377983e0
benibogyo <benjamin.katona@yahoo.com>
2022-02-14 10:58:17 +0100

    a belépő felhasználó gombok megjelennek

--- src/main/java/hu/progmatic/kozos/felhasznalo/LoginController.java
+++ src/main/java/hu/progmatic/kozos/felhasznalo/LoginController.java
@@ -2,6 +2,7 @@ package hu.progmatic.kozos.felhasznalo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,8 +12,23 @@ import java.util.List;
 public class LoginController {
 
     @RequestMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        getFelhasznaloGombLista(model);
         return "etterem/belepes";
+    }
+
+    private void getFelhasznaloGombLista(Model model) {
+        List<Felhasznalo> felhasznaloList = List.of(
+                new Felhasznalo(null, "admin", "adminpass", UserType.ADMIN),
+                new Felhasznalo(null, "bence", "bence", UserType.ADMIN),
+                new Felhasznalo(null, "benji", "benji", UserType.ADMIN),
+                new Felhasznalo(null, "attila", "attila", UserType.ADMIN),
+                new Felhasznalo(null, "olivér", "olivér", UserType.ADMIN),
+                new Felhasznalo(null, "dávid", "dávid", UserType.ADMIN),
+                new Felhasznalo(null, "miska", "miska", UserType.ADMIN),
+                new Felhasznalo(null, "felszolgáló", "felszolgáló", UserType.FELSZOLGALO)
+        );
+        model.addAttribute("felhasznaloGombList", felhasznaloList);
     }
 
     @ModelAttribute("felhasznaloGombList")
